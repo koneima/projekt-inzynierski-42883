f77ed7a952015d8b2ca00baeb8a0fd1542eb84ce
koneima <mkoniecznaa00@gmail.com>
2025-11-30 22:09:19 +0100

    cała strona

--- src/main/java/com/example/projektinzynierski42883/ProjektInzynierski42883Application.java
+++ src/main/java/com/example/projektinzynierski42883/ProjektInzynierski42883Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjektInzynierski42883Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjektInzynierski42883Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektInzynierski42883Application.class, args);
+    }
 
 }
